8913f35ac85cdbce91b1549b04fc96380ac9f1ad
henrique-serra <66788126+henrique-serra@users.noreply.github.com>
2025-02-07 18:28:26 +0000

    Add modules

---
+++ script.js
@@ -0,0 +1,120 @@
+function Gameboard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    // Set initial board
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(Cell());
+        }
+    }
+
+    const getBoard = () => board;
+
+    const printBoard = () => {
+        const boardWithCellValues = board.map(row => row.map(cell => cell.getValue()));
+        console.table(boardWithCellValues);
+    }
+
+    const markCell = (player, row, column) => {
+        const cell = board[row][column];
+        cell.updateCellValue(player);
+    }
+
+    return {
+        getBoard,
+        printBoard,
+        markCell
+    }
+}
+
+function Cell() {
+    let value = null;
+
+    const getValue = () => value;
+    const updateCellValue = (player) => {
+        value = player.marker;
+    };
+
+    return {
+        getValue,
+        updateCellValue
+    };
+}
+
+function Player(name, marker) {
+    return {
+        name,
+        marker
+    }
+}
+
+function GameController(
+    player1 = Player("Player 1", "O"),
+    player2 = Player("Player 2", "X")
+) {
+    
+    let activePlayer = player1;
+
+    const board = Gameboard();
+
+    const getActivePlayer = () => activePlayer;
+
+    const switchPlayers = () => {
+        activePlayer = activePlayer === player1 ? player2 : player1;
+    }
+
+    const verticalWin = (column) => {
+        const verticalMarkers = [];
+        for (row of board.getBoard()) {
+            verticalMarkers.push(row[column].getValue());
+        }
+        const uniqueMarkers = new Set(verticalMarkers);
+        return uniqueMarkers.size == 1 ? true : false;
+    }
+
+    const horizontalWin = (row) => {
+
+    }
+
+    const diagonalWin = () => {
+        
+    }
+
+    const win = () => {
+
+    }
+
+    const draw = () => {
+
+    }
+
+    const playRound = () => {
+        let [row, column] = prompt("row, column").split(",");
+        
+        if (row > 2 || column > 2) {
+            alert("Off the limits!");
+            return;
+        }
+
+        const cell = board.getBoard()[row][column];
+        
+        if (cell.getValue()) {
+            alert("Cell taken! Choose another");
+            return;
+        }
+
+
+        board.markCell(activePlayer, row, column);
+        board.printBoard();
+        switchPlayers();
+    }
+
+    return {
+        playRound,
+        getActivePlayer,
+        verticalWin
+    }
+}
